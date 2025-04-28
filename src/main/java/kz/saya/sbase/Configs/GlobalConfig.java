32d01ff2dbf34f17295ee2d7e797a7c7cebcda95
Saya <nyeltaev133@gmail.com>
2025-04-28 18:55:37 +0500

    Got rid of GraphQL

--- src/main/java/kz/saya/sbase/Configs/GlobalConfig.java
+++ src/main/java/kz/saya/sbase/Configs/GlobalConfig.java
@@ -1,9 +1,7 @@
 package kz.saya.sbase.Configs;
 
-import graphql.scalars.ExtendedScalars;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.graphql.execution.RuntimeWiringConfigurer;
 
 @Configuration
 public class GlobalConfig {
